89d3fcba878c7d4a15925de20d07a11877ca7fac
Vanza Setia <venusbumi2@gmail.com>
2021-11-03 11:31:36 +0700

    Added toggle functionality to the share button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 (function () {
-  'use strict';
-  console.log('Hello World');
+  "use strict";
+  const cardShareToggleButton = document.querySelector(
+    ".js-cardShareToggleButton"
+  );
+
+  const toggleShareOptionsElement = () => {
+    const shareOptionsElement = document.querySelector(
+      ".js-shareOptionsElement"
+    );
+
+    if (shareOptionsElement.hasAttribute("hidden")) {
+      shareOptionsElement.removeAttribute("hidden");
+      cardShareToggleButton.classList.add("is-active");
+    } else {
+      shareOptionsElement.setAttribute("hidden", "");
+      cardShareToggleButton.classList.remove("is-active");
+    }
+  };
+
+  cardShareToggleButton.addEventListener(
+    "click",
+    toggleShareOptionsElement
+  );
 })();
